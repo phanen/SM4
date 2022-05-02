48c9d0f428c18e8dec346c6e91d95fe522e84d60
phan <N2463670458@outlook.com>
2022-05-02 14:24:10 +0800

    <finished>         basic-implementation, multithread-implementation <unfinished>    SIMD-implementation

---
+++ wk.cpp
@@ -0,0 +1,10 @@
+//#include<bits\stdc++.h>
+//
+//using namespace std;
+//
+//int main() {
+//	for (size_t i = 0; i < 32; i++)
+//	{
+//		printf("rk[%lld] = rk[%lld]^ (sm4_func_T_key(rk[%lld] ^ rk[%lld] ^ rk[%lld] ^ CK[%lld]));\n", i + 4, i, i + 1, i + 2, i + 3, i + 4);
+//	}
+//}
